b85616fe140d3b2a297fe9fe085dd8da406053be
saicharan <saicharan.reddy1@gmail.com>
2018-05-14 05:27:24 +0530

    Scraped Best Buy for deals delivery

--- deals.py
+++ deals.py
@@ -1,27 +1,33 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
 import bs4 as bs
-from urllib.request
+import urllib.request
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 source = urllib.request.urlopen('https://www.bestbuy.com/site/clp/sale-page/pcmcat185700050011.c?id=pcmcat185700050011').read()
 soup = bs.BeautifulSoup(source,'lxml')
 
-@ask.launch
-def launched():
-    return question('Welcome to BestBuy Deals.')
-
-@ask.intent('InvokeIntent')
-def invoke():
-    text = render_template('welcome')
-    return statement(text).simple_card('welcome', text)
-
-@ask.intent('DealsIntent')
-def deals():
-    for deal in soup.find_all('h3',attrs={'class':'offer-link'}):
+def GetDeals():
+    for deal in soup.find_all('h3',attrs={"class":"offer-link"}):
         return statement(deal.text)
 
+
+@ask.launch
+def launched():
+    welcome_msg = "Welcome to BestBuy Deals. Are you interested in today's deals?"
+    return question(welcome_msg)
+
+@ask.intent('YesIntent')
+def ShareDeals():
+    deals = GetDeals()
+    deals_msg = "The top deals for today are {}".format(deals)
+    return statement(deals)
+
+@ask.intent('NoIntent')
+def NoIntent():
+    exit_text = 'Okay then! Bye!'
+    return statement(exit_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
